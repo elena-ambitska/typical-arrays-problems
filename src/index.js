52653d1243f011b3208f2611cd6594454bf0bc66
Lena <lambitska92@gmail.com>
2021-02-14 14:22:56 +0200

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+  
+  if (Array.isArray(array) && array.length>0){
+  return Math.min(...array);
+  }else{
+    return 0
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (Array.isArray(array) && array.length>0){
+  return Math.max(...array);
+}else{
+  return 0
+}
 }
 
+
+
+
+
 exports.avg = function avg (array) {
-  return 0;
-}
+  if (Array.isArray(array) && array.length>0){
+    let sum=array.reduce((a, b) => a + b, 0);
+    let avg=sum / array.length;
+    return  avg;
+  }else{
+    return 0
+  }
+  }
